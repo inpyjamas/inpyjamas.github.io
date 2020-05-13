cec4a2dee38e27a48d99908db77fb51b27bb91c3
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-05-13 10:14:26 +0200

    fix(eleventy): fix eleventy setup
    still want the source in a subfolder but for now

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,16 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setTemplateFormats(["liquid", "html"]);
-  eleventyConfig.addWatchTarget("./src/assets/css/*.css");
-  eleventyConfig.addPassthroughCopy({ "src/assets/css/": "assets/css/" });
+  eleventyConfig.setTemplateFormats(["html"]);
+  eleventyConfig.addWatchTarget("./assets/css/*.css");
+  eleventyConfig.addPassthroughCopy("assets/css/");
   return {
-    input: "src",
-    includes: "_includes",
-    layouts: "_layouts",
-    output: "_site",
-    passthroughFileCopy: true,
+    // input: "src",
+    // includes: "_includes",
+    // // includes: "_includes",
+    // // layouts: "_layouts",
+    // output: "_site",
+    // passthroughFileCopy: true,
+    htmlTemplateEngine: "liquid",
+    // markdownTemplateEngine: "liquid",
+    // htmlTemplateEngine: "liquid",
   };
 };
